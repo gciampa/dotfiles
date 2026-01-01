d01999dcbc987350224ffcd3a87eee663fbb1163
Greg Ciampa <gciampa@gmail.com>
2026-01-01 17:11:45 -0500

    Pin nvim-treesitter to master

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,20 @@
 -- Highlight, edit, and navigate code
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'master', -- Use stable branch (main is a breaking rewrite)
   build = ':TSUpdate',
+  main = 'nvim-treesitter.configs',
   dependencies = {
-    'RRethy/nvim-treesitter-endwise', -- Auto-add 'end' for Ruby blocks
+    'RRethy/nvim-treesitter-endwise',
   },
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter').setup {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'ruby', 'embedded_template', 'javascript', 'typescript', 'json' },
-      auto_install = true,
-    }
-    -- Enable treesitter-based highlighting
-    vim.api.nvim_create_autocmd('FileType', {
-      callback = function(args)
-        pcall(vim.treesitter.start, args.buf)
-      end,
-    })
-    -- Ruby uses vim regex for indentation
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'ruby',
-      callback = function()
-        vim.bo.indentexpr = ''
-      end,
-    })
-  end,
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  opts = {
+    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'ruby', 'embedded_template', 'javascript', 'typescript', 'json' },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { 'ruby' },
+    },
+    indent = { enable = true, disable = { 'ruby' } },
+    endwise = { enable = true },
+  },
 }
